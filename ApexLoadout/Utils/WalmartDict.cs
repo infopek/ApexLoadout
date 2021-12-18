7d3d6a198e16c6434189dd32be014eb4118f59c0
infopek <10saero01@gmail.com>
2021-12-18 22:50:35 +0100

    Create assault rifle weapons

---
+++ ApexLoadout/Utils/WalmartDict.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ApexLoadout.Utils
+{
+	class WalmartDict
+	{
+
+	}
+}
--- ApexLoadout/Weapons/AssaultRifle.cs
+++ ApexLoadout/Weapons/AssaultRifle.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ApexLoadout.Weapons
 {
-	class AssaultRifle
+	/// <summary>
+	/// Class for representing an assault rifle type weapon
+	/// </summary>
+	class AssaultRifle : Weapon
 	{
+		static string[] weapons = { "HAVOC Rifle", "VK-47 Flatline", "Hemlok Burst AR", "R-301 Carbine" };
+
+		public string GetWeapon(int index)
+		{
+			return weapons[index];
+		}
 	}
 }
