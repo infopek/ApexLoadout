d8989c2e54be7029896630979060250b4fcc1fb1
infopek <10saero01@gmail.com>
2021-12-20 13:16:19 +0100

    Fixes

--- ApexLoadout/Program.cs
+++ ApexLoadout/Program.cs
@@ -7,56 +7,26 @@ namespace ApexLoadout
 	{
 		static Random rand = new();
 
-		static Weapon NumToType(int n)
-		{
-			Weapon w;
-
-			switch (n)
-			{
-				case 0:
-					w = new Pistol();
-					break;
-				case 1:
-					w = new Shotgun();
-					break;
-				case 2:
-					w = new Sniper();
-					break;
-				case 3:
-					w = new Marksman();
-					break;
-				case 4:
-					w = new LightMachineGun();
-					break;
-				case 5:
-					w = new SubmachineGun();
-					break;
-				case 6:
-					w = new AssaultRifle();
-					break;
-				default:
-					w = new();
-					break;
-			}
-
-			return w;
-		}
-
 		static void Main(string[] args)
 		{
-			int firstType = rand.Next(0, 7);
-			int secondType = rand.Next(0, 7);
-
-			while (secondType == firstType)
+			// Next 10 rounds
+			for (int i = 0; i < 10; i++)
 			{
-				secondType = rand.Next(0, 7);
+				int firstType = rand.Next(0, 7);
+				int secondType = rand.Next(0, 7);
+
+				while (secondType == firstType)
+				{
+					secondType = rand.Next(0, 7);
+				}
+
+
+				CarePackage care = new();
+
+				string[] carePacWeps = care.Allowed(rand);
+
+				Console.WriteLine($"{i}. round: {}");
 			}
-
-			Weapon slotOne = NumToType(firstType);
-			Weapon slotTwo = NumToType(secondType);
-			CarePackage care = new();
-
-			string[] carePacWeps = care.Allowed(rand);
 		}
 	}
 }
