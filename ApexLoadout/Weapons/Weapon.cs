5f5199de715e73c8639bf2d1e8907b86b2aaeb64
infopek <10saero01@gmail.com>
2021-12-21 15:44:02 +0100

    Add enum for weapon category

--- ApexLoadout/Weapons/Weapon.cs
+++ ApexLoadout/Weapons/Weapon.cs
@@ -3,20 +3,34 @@
 namespace ApexLoadout.Weapons
 {
 	/// <summary>
+	/// Enum for representing certain weapon categories
+	/// </summary>
+	enum Category
+	{
+		AssaultRifle = 1,
+		SubmachineGun = 2,
+		LightMachineGun = 3,
+		Marksman = 4,
+		Sniper = 5,
+		Shotgun = 6,
+		Pistol = 7
+	}
+
+	/// <summary>
 	/// Class for a generic non-melee weapon in Apex Legends
 	/// </summary>
 	class Weapon
 	{
 		string name;
-		string category;
+		Category category;
 		
 		public string Name { get => name; set => name = value; }
-		public string Category { get => category; set => category = value; }
+		public Category Category { get => category; set => category = value; }
 
-		public Weapon(string name, string category)
+		public Weapon(string name, int category)
 		{
 			this.name = name;
-			this.category = category;
+			this.category = (Category)category;
 		}
 	}
 }
