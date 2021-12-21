9a0468f8cc05d4e193bc18396677620a67c2f3ac
infopek <10saero01@gmail.com>
2021-12-21 16:43:51 +0100

    Add 2d-array to store weapons

--- ApexLoadout/Program.cs
+++ ApexLoadout/Program.cs
@@ -9,7 +9,17 @@ namespace ApexLoadout
 
 		static void Main(string[] args)
 		{
-			
+			Weapon[,] weapons =
+			{
+				{ new Weapon("Alti", 0), new Weapon("Spitticus", 0), new Weapon("Scut", 0), new Weapon("Krab", 0) },
+				{ new Weapon("HAVOK", 1), new Weapon("Flatlin", 1), new Weapon("Homlok", 1), new Weapon("R-301", 1) },
+				{ new Weapon("Prowla", 2), new Weapon("R-9", 2), new Weapon("Vilt", 2), new Weapon("Car", 2) },
+				{ new Weapon("Devo", 3), new Weapon("W-Star", 3), new Weapon("Page", 3), null },
+				{ new Weapon("TakeTakeTake", 4), new Weapon("Peti", 4), new Weapon("Bocsek", 4), null },
+				{ new Weapon("ChargeRifla", 5), new Weapon("Longboo", 5), new Weapon("Senti", 5), null },
+				{ new Weapon("EVA", 6), new Weapon("Stiffy", 6), new Weapon("Bique", 6), new Weapon("PK", 6) },
+				{ new Weapon("REE", 7), new Weapon("P20", 7), new Weapon("Wingdud", 7), null }
+			};
 		}
 	}
 }
